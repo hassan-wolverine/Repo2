75682e8cd27a49de46888bb09599b03fc2cccbdc
hassan-wolverine <43892981+hassan-wolverine@users.noreply.github.com>
2021-11-06 13:58:45 +0200

    added a comment at the end of main

--- template pattern/src/MainClass.java
+++ template pattern/src/MainClass.java
@@ -3,8 +3,9 @@ public class MainClass {
     public static void main(String[] args) {
 
         System.out.println("7mada");
-    // comment
+        // comment
         System.out.println("7mada2");
+        // another comemnt
     }
 
 }
